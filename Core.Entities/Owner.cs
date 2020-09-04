f33a7e0744253c6801fa5fa0a6860f76f7a4b058
Rado <radoslav.backovsky@gmail.com>
2020-09-04 10:28:45 +0200

    Owner BE created

--- Core.Entities/Owner.cs
+++ Core.Entities/Owner.cs
@@ -7,11 +7,17 @@ namespace PetShop.Core.Entities {
 
     class Owner {
 
+        public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
 
         public string Address { get; set; }
 
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
 
     }
 }
